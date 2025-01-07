92181578668ab0e1f0f96aff1999731b898b026a
bub <57535312+bubtheengineer@users.noreply.github.com>
2025-01-07 11:07:31 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import outputs from "../amplify_outputs.json";
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_lcncLDwk2",
   client_id: "u6ui0mp0go2ue7dsam1j9bpon",
-  redirect_uri: "https://us-east-1lcncldwk2.amazoncognito.com/oauth2/idpresponse",
+  redirect_uri: "https://main.d3hee5mxmxchnd.amplifyapp.com/",
   response_type: "code",
   scope: "openid",
 };
