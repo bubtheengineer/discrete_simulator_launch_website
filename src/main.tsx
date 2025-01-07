0a417ce3ac0e4fb5c67da0cbe7a49aca5eaf6e3c
bub <57535312+bubtheengineer@users.noreply.github.com>
2025-01-07 11:06:33 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AuthProvider } from "react-oidc-context";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_lcncLDwk2",
+  client_id: "u6ui0mp0go2ue7dsam1j9bpon",
+  redirect_uri: "https://us-east-1lcncldwk2.amazoncognito.com/oauth2/idpresponse",
+  response_type: "code",
+  scope: "openid",
+};
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
